bad829587ea265f4f2a090a6d4700fdbe8f83bda
Kostiantyn Skliar <skljar.k@gmail.com>
2023-05-30 11:45:14 +0300

    Creating project structure. Packages added: data, pages, utilities, tests_ui. Files added: locators.py, pages.py, application.py, tests_ui.py, conftest.py, pytest.ini, requirements.txt.

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import logging
+import sys
+
+from pytest import fixture
+from reportportal_client import RPLogger
+from playwright.sync_api import sync_playwright
+
+# from utilities.application import App
+
+
+# Report Portal
+@fixture(scope="session")
+def rp_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logging.setLoggerClass(RPLogger)
+    return logger
+
+
+# Playwright
+@fixture(scope="session")
+def get_playwright():
+    with sync_playwright() as p:
+        yield p
+
+
+@fixture
+def driver(get_playwright, request):
+    base_url = request.config.getini("base_url")    # Setup in pytest.ini
+    app = App(get_playwright, base_url=base_url)
+    app.goto("/")
+    yield app
+    app.close()
