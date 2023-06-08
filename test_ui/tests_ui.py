8bc3a909733dfeddf888b9797148ea037e637107
Kostiantyn Skliar <skljar.k@gmail.com>
2023-06-08 17:46:55 +0300

    Create test for login user with valid data.

--- test_ui/tests_ui.py
+++ test_ui/tests_ui.py
@@ -17,6 +17,27 @@ class TestsRegistrationPage:
 
         assert m.check_sidebar_is_present() == True
 
+
+class TestsSignInPage:
+
+    def test_login_user_with_valid_data(self, driver):
+        m = driver.main_page
+        s = driver.sign_in_page
+        r = driver.registration_page
+        user = Users().static_user
+
+        m.navigate_to_sign_in_page_by_header_menu()
+        try:
+            s.login(user, user)
+            if s.sign_on_failed_message_is_visible() is True:
+                s.navigate_to_register_page_by_link()
+                r.filling_in_registration_form(user)
+                r.send_new_user_information()
+        finally:
+            # TODO: fix it
+            # s.login(user, user)
+            assert m.check_user_is_login() == user  # 'Jessica'
+
     def test_user(self):
         user = Users().account_info()
         np = str(user)
